03b1957fe033fa4a2193143ff60cdbbfe3b1c6ed
Despedo <despedo@gmail.com>
2017-03-19 00:05:47 +0200

    Created MainActivity and activity_main layout, configured manifest file

--- app/src/main/java/com/despedo/rss_msrcedes/MainActivity.java
+++ app/src/main/java/com/despedo/rss_msrcedes/MainActivity.java
@@ -1,8 +1,17 @@
 package com.despedo.rss_msrcedes;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 /**
  * Created by Despedo on 18.03.2017.
  */
 
-public class MainActivity {
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
