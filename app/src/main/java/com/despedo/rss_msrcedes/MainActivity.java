634f5eddfb0357295e8ef9f9ff7b5569b5900eff
Despedo <despedo@gmail.com>
2017-03-19 17:27:04 +0200

    Added toolbar, search button, fixed styles

--- app/src/main/java/com/despedo/rss_msrcedes/MainActivity.java
+++ app/src/main/java/com/despedo/rss_msrcedes/MainActivity.java
@@ -1,17 +1,43 @@
 package com.despedo.rss_msrcedes;
 
-import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
 
 /**
  * Created by Despedo on 18.03.2017.
  */
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+//        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        initToolbar();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.news);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
