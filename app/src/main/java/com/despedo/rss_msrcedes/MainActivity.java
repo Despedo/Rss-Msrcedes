a444b95f2b64ae79589b5343e7012ffaf91f40fd
Despedo <despedo@gmail.com>
2017-03-19 18:58:13 +0200

    Filled navigationView

--- app/src/main/java/com/despedo/rss_msrcedes/MainActivity.java
+++ app/src/main/java/com/despedo/rss_msrcedes/MainActivity.java
@@ -4,9 +4,11 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 
 
 /**
@@ -15,16 +17,24 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int LAYOUT = R.layout.activity_main;
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
+        setContentView(LAYOUT);
+
         initToolbar();
+        initNavigationView();
+    }
+
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
